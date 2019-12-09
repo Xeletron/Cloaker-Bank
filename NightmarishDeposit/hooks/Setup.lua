4dd1c91e455fa108e45adb9b0ef0f9e5d50a27be
Xeletron <xeletron1@gmail.com>
2019-12-09 16:01:30 +0100

    Added Drilling Necro, Fixed some things dissapearing

--- NightmarishDeposit/hooks/Setup.lua
+++ NightmarishDeposit/hooks/Setup.lua
@@ -2,12 +2,12 @@ Hooks:Add('NetworkReceivedData', 'NetworkReceivedData_CLK', function(sender, mes
 	if messageType == "clkCutscene" then
 		local decode = json.decode(data)
 		local position = data[1]
-		local pos = Vector3(position[1], position[2], position[3])
+		local pos = position and Vector3(position[1], position[2], position[3]) or Vector3()
 		local rotation = data[2]
-		local rot = Rotation(rotation[1], rotation[2], rotation[3])
+		local rot = rotation and Rotation(rotation[1], rotation[2], rotation[3]) or Rotation()
 		local duration = data[3]
 		local player = managers.player:player_unit()
-		if alive(player) then
+		if alive(player) and position and rotation then
 			player:camera()._camera_unit:base():set_cutscene(pos, rot, duration)
 		end
 	end
